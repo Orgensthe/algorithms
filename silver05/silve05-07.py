25b72bdf5f785de16e44151409c22204b020c8e1
jeff <jeff@innogrid.com>
2020-01-27 22:49:37 +0900

    1475 silver05-07 solved

--- silver05/silve05-07.py
+++ silver05/silve05-07.py
@@ -5,17 +5,15 @@ def solution():
     room_number = list(input())
     room_number
     count = Counter(room_number) 
+    num_of_6_and_9 = math.ceil( (count['6'] + count['9'])/2)
+    count['6']  = num_of_6_and_9
+    count['9']  = num_of_6_and_9
+
     max_key = count.most_common(1)
-    
-    if max_key[0][0] == '9' or int(max_key[0][0]) == '6' :
-      
-        print(math.ceil( (count['6'] + count['9'])/2) )
-        return
-    
-    if  count[max_key[0][0]] < (count['6'] + count['9'])//2 :
-        print(math.ceil( (count['6'] + count['9'])/2) )
-    else:
-        print(max_key[0][1])
+
+
+
+    print(max_key[0][1])
 
 
 solution()
