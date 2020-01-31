4c59c4935c8f017a02374cc58966fc921e773702
orgensthe <w4102945@gmail.com>
2020-01-31 09:55:25 +0900

    1476 silver05-10 solved

--- silver05/silver05-10.py
+++ silver05/silver05-10.py
@@ -4,9 +4,7 @@ def solutuon():
     n1,n2,n3 = 532,285,420
     s1 , s2 ,s3 = 13,17,10
 
-
     result = (int(a1)*n1*s1 + int(a2)*n2*s2 + int(a3)*n3*s3) %7980
-
 
     if result == 0:
         print(7980)
