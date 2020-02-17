2b1db00744ed33568e9991a8986ec478eacc297e
orgensthe <w4102945@gmail.com>
2020-02-17 09:42:19 +0900

    DP01 1로 만들기 solved

---
+++ DP/DP-01.py
@@ -0,0 +1,32 @@
+import sys
+
+num = int(sys.stdin.readline().strip())
+
+list_ = [0 for i in range(num+1)]
+
+
+def min(x,y):
+    return x if x >= y else y
+
+def num_check(target):
+    
+    list_[1] = 0
+
+    for i in range(2,target+1):
+        
+        if i == 2 or i == 3:
+            list_[i] = 1
+        else:
+            list_[i] = list_[i-1]+1
+            min = list_[i-1]
+            if i %3 ==0:
+                min = list_[i//3] if  list_[i//3] <= min else min
+                list_[i] = min+1
+            if i % 2 == 0:
+                min = list_[i//2] if  list_[i//2] <= min else min
+                list_[i] = min+1
+
+    print(list_[target])
+   
+
+num_check(num)
---
+++ silver05/silver05-22.py
@@ -0,0 +1,13 @@
+import sys
+num = int(sys.stdin.readline().strip())
+restult = 0
+check = 0
+
+while num != restult:
+    
+    check += 1
+    if '666' in str(check):
+        restult+=1
+
+    
+print(check)
