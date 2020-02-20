597587746a9a278e02fc509a0499a9b896cdf272
orgensthe <w4102945@gmail.com>
2020-02-21 08:28:20 +0900

    DP-05 16194 카드 구매하기 solved

--- DP/DP-05.py
+++ DP/DP-05.py
@@ -7,12 +7,13 @@ card_list =  list(map(int ,sys.stdin.readline().strip().split(" ")))
 
 
 def solution(num):
-    min_price = [ 10000000 for i in range(num)]
+    min_price = [ 10000000 for i in range(num+1)]
 
-    min_price[0] = card_list[0]
+    min_price[0] = 0
     
-    for i in range(1,num):
+    for i in range(1,num+1):
         for j in range(1,i+1):
             min_price[i] =  min(min_price[i], min_price[i-j]+card_list[j-1])
-    print(min_price[num-1])
+
+    print(min_price[num])
 solution(num)
