a72a9fb0a42da6fabd75bba28aa69d52375866de
orgensthe <w4102945@gmail.com>
2020-03-25 09:05:41 +0900

    graph01 13023 ABCDE solving

--- graph/graph01.py
+++ graph/graph01.py
@@ -1,35 +1,26 @@
 import sys
-from collections import deque
 
 people , numFirend = map(int,sys.stdin.readline().strip().split(" "))
 lineList = [[] for x in range(people)]
 check = [ False for x in range(people)]
 
-
-
 for i in range(numFirend):
     startpoint , endpoint = map(int,sys.stdin.readline().strip().split(" "))
     lineList[startpoint].append((startpoint,endpoint))
+    lineList[endpoint].append((endpoint,startpoint))
 
 
-q = deque()
-q.append(0)
-check[0] = True
-count = 0
-while q:
-    point = q.popleft()
-    print(q)
-    for _set in lineList[point]:
-        if not check [_set[1]] :
-            check [_set[1]] = True
-            count +=1
-            q.append(_set[1])
+def dfs(point, depth):
+    if depth >= 4 : return True
+    if check[point] == True: return
 
+    check[point] = True
+    for line in lineList[point] :
+        if (dfs(line[1],depth+1)):
+            return True
 
-if count >= 4:
-    print(1)
-else:
-    print(0)
-
-
-
+for i in range(people):
+    if dfs(i,0):
+        print(1) 
+        break
+    if i == people-1: print(0) 
