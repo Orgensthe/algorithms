f2daf3c32937a5563e0a6fabb36e0241c715df49
Jeff <jeff@Jeffui-MacBookPro.local>
2020-05-04 18:44:43 +0900

    SockMerchant hackeranck WarmUp01

---
+++ Hackeranck/warmup/CountingValleys.py
@@ -0,0 +1,8 @@
+def countingValleys(n, s):
+
+
+    pass
+
+
+
+countingValleys(9,"UDDDUDUU")
---
+++ Hackeranck/warmup/SockMerchant.py
@@ -0,0 +1,19 @@
+import sys
+
+num = sys.stdin.readline()
+
+socks = list(map(int, sys.stdin.readline().strip().split(" ")))
+
+dic = dict()
+for sock in socks:
+    try:
+        dic[sock] +=1
+    except KeyError:
+        dic[sock] = 1
+
+result = 0
+
+for key in dic.keys():
+    result += (dic[key]//2)
+
+print(result)
